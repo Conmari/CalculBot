096c4c4e43d834e92cfc63595fd395af4a143dd9
wasilisk <walbynow@gmail.com>
2023-08-04 08:23:03 +0300

    добаленно в main.py провепка ввода на число

--- python/main.py
+++ python/main.py
@@ -15,6 +15,7 @@ async def set_default_commands(dp):
 
 @bot.message_handler(commands=['calculator'])
 def calc(message):
+    # bot.send_message(message.chat.id, 'Сейчас меня фиксят временно не отвечаю)')
     numan = bot.send_message(message.chat.id, 'Первое число')
     bot.register_next_step_handler(numan, num1_fun)
 
@@ -22,56 +23,70 @@ def calc(message):
 def num1_fun(message):
     global num1;
     num1 = message.text
-    numtwo = bot.send_message(message.chat.id, 'Второе число')
-    bot.register_next_step_handler(numtwo, num2_fun)
+    if not num1.isdigit():
+        bot.send_message(message.chat.id, 'Вы ввели не число')
+        bot.send_message(message.chat.id, 'Пожалуйста повторите попытку c самого начала')
+    else:
+        numtwo = bot.send_message(message.chat.id, 'Второе число')
+        bot.register_next_step_handler(numtwo, num2_fun)
+
+
 
 
 def num2_fun(message):
     global num2;
     num2 = message.text
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    summ = types.KeyboardButton('Сложение (+)')
-    subtraction = types.KeyboardButton('Вычитание (-)')
-    multiply = types.KeyboardButton('Умножение (*)')
-    delete = types.KeyboardButton('Деление (/)')
+    if not num2.isdigit():
+        bot.send_message(message.chat.id, 'Вы ввели не число')
+        bot.send_message(message.chat.id, 'Пожалуйста повторите попытку c самого начала')
 
-    markup.add(summ, subtraction, multiply, delete)
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        summ = types.KeyboardButton('Сложение (+)')
+        subtraction = types.KeyboardButton('Вычитание (-)')
+        multiply = types.KeyboardButton('Умножение (*)')
+        delete = types.KeyboardButton('Деление (/)')
 
-    operu = bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
+        markup.add(summ, subtraction, multiply, delete)
 
-    bot.register_next_step_handler(operu, operi)
+        operu = bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
+
+        bot.register_next_step_handler(operu, operi)
 
 
 def operi(message):
     global oper;
     oper = message.text
-    bot.send_message(message.chat.id, 'Результат:', reply_markup=types.ReplyKeyboardRemove())
-    if oper == "Сложение (+)" or oper == '+':
-        res = logik.summ(num1, num2)
-        bot.send_message(message.chat.id, res)
-    elif oper == "Вычитание (-)" or oper == '-':
-        res = logik.subtraction(num1, num2)
-        bot.send_message(message.chat.id, res)
-    elif oper == "Умножение (*)" or oper == '*':
-        res = logik.multiply(num1, num2)
-        bot.send_message(message.chat.id, res)
-    elif oper == "Деление (/)" or oper == '/':
-        try:
-            res = logik.delete(num1, num2)
+    if not oper.isdigit():
+        bot.send_message(message.chat.id, 'Результат:', reply_markup=types.ReplyKeyboardRemove())
+        if oper == "Сложение (+)" or oper == '+':
+            res = logik.summ(num1, num2)
             bot.send_message(message.chat.id, res)
-        except ZeroDivisionError:
-            bot.send_message(message.chat.id, "Деление на ноль невозможно")
+        elif oper == "Вычитание (-)" or oper == '-':
+            res = logik.subtraction(num1, num2)
+            bot.send_message(message.chat.id, res)
+        elif oper == "Умножение (*)" or oper == '*':
+            res = logik.multiply(num1, num2)
+            bot.send_message(message.chat.id, res)
+        elif oper == "Деление (/)" or oper == '/':
+            try:
+                res = logik.delete(num1, num2)
+                bot.send_message(message.chat.id, res)
+            except ZeroDivisionError:
+                bot.send_message(message.chat.id, "Деление на ноль невозможно")
+        else:
+            bot.send_message(message.chat.id, "Ошибка")
     else:
-        bot.send_message(message.chat.id, "Ошибка")
+        bot.send_message(message.chat.id, "Ошибка1")
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Приветик' + message.from_user.first_name)
+    bot.send_message(message.chat.id, 'Приветик ' + message.from_user.first_name)
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['info'])
 def main(message):
     bot.send_message(message.chat.id, 'Сейчас я очень мал всё что мне подвластны основные операции напиши /calculator и следуй инструкциям')
 
